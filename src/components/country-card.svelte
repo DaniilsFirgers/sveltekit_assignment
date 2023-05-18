26b134e5fd43868f09ded650f18ee9af9d53aef1
Daniils Firgers <dfirger@gmail.com>
2023-05-18 21:13:51 +0300

    add country card component

---
+++ src/components/country-card.svelte
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+  export let countryName: string;
+  export let imgUrl: string;
+</script>
+
+<div class="border rounded-lg w-fit p-2 bg-base-200">
+  <h1 class="text-lg font-medium text-center text-primary-focus">
+    {countryName}
+  </h1>
+  <img src={imgUrl} alt={`${countryName}-flag`} class="h-40 w-80 rounded-lg" />
+</div>
