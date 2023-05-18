c1be0c0ad8e025199297ce7d9d8dc7ed1a3afd4e
Daniils Firgers <dfirger@gmail.com>
2023-05-18 21:13:58 +0300

    add footer component

---
+++ src/components/footer.svelte
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+  import moment from "moment-timezone";
+  const datetime = moment().tz("EET").format("DD.MM.YYYY");
+</script>
+
+<footer class="flex justify-center bg-accent-focus">
+  <div class="flex justify-center items-center">
+    <h1 class="text-lg font-medium">{datetime}</h1>
+  </div>
+</footer>
