30c4483046d6723111816b3082b3d1524bb0ae87
Daniils Firgers <dfirger@gmail.com>
2023-05-18 21:15:43 +0300

    fix layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,55 +1,15 @@
 <script>
-  import Header from "./Header.svelte";
+  import Header from "../components/header.svelte";
+  import Footer from "../components/footer.svelte";
   import "../tailwind.css";
 </script>
 
-<div class="app">
+<div class="flex flex-col min-h-screen">
   <Header />
 
-  <main>
+  <main class="flex flex-1 flex-col">
     <slot />
   </main>
 
-  <footer>
-    <p>
-      visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit
-    </p>
-  </footer>
+  <Footer />
 </div>
-
-<style>
-  .app {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-  }
-
-  main {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    padding: 1rem;
-    width: 100%;
-    max-width: 64rem;
-    margin: 0 auto;
-    box-sizing: border-box;
-  }
-
-  footer {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 12px;
-  }
-
-  footer a {
-    font-weight: bold;
-  }
-
-  @media (min-width: 480px) {
-    footer {
-      padding: 12px 0;
-    }
-  }
-</style>
