ac57ba5e12fe94e0464b3d1d89d1ca11b8f65006
Daniils Firgers <dfirger@gmail.com>
2023-05-18 21:14:06 +0300

    add heder compoennt

---
+++ src/components/header.svelte
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+</script>
+
+<header class="flex justify-between bg-secondary-focus">
+  <div class="flex justify-center items-center">
+    <h1 class="text-lg font-medium pl-4 text-primary-focus">Travelify</h1>
+  </div>
+  <div class="flex justify-center items-center gap-4">
+    <a href="/home" class="bg-secondary p-1 border rounded-md">Home</a>
+    <a href="/countries" class="bg-secondary p-1 border rounded-md">Countries</a
+    >
+    <a href="/about" class="bg-secondary p-1 border rounded-md">About</a>
+  </div>
+
+  <div>
+    <a href="https://github.com/DaniilsFirgers/sveltekit_assignment">
+      <img src="/utils/github.svg" alt="github logo" class="h-16 w-16" />
+    </a>
+  </div>
+</header>
