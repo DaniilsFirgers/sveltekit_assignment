7ef2e0d4ae59eb0ee416f929ab7af409dbac7a39
Daniils Firgers <dfirger@gmail.com>
2023-05-18 21:16:43 +0300

    refactor multiple files in routes

--- src/routes/countries/+page.svelte
+++ src/routes/countries/+page.svelte
@@ -1,7 +1,24 @@
 <script lang="ts" setup>
+  import CountryCard from "../../components/country-card.svelte";
+  import { countriesStore } from "../../stores/countries";
+
+  let countries_list = getCountriesData();
+
+  async function getCountriesData() {
+    const res = await $countriesStore;
+    return res.countries;
+  }
+  getCountriesData();
 </script>
 
-<main>
-  <img src="/flags/canada.png" alt="" class="h-20 w-20" />
-  <div class="h-20 w-20 bg-red-500" />
+<main class="flex justify-center items-center">
+  {#await countries_list}
+    <h1>Waiting for countries data...</h1>
+  {:then countries}
+    <div class="flex justify-center items-center grid grid-cols-2 gap-10 p-2">
+      {#each countries as country}
+        <CountryCard countryName={country.name} imgUrl={country.flag_url} />
+      {/each}
+    </div>
+  {/await}
 </main>
