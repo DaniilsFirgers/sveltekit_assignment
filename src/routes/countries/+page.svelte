82efa211742bbb6c62ed3f70674d9f9c3a77d414
Daniils Firgers <dfirger@gmail.com>
2023-05-24 22:48:21 +0300

    add goto and navifation to page

--- src/routes/countries/+page.svelte
+++ src/routes/countries/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+  import { goto } from "$app/navigation";
+  import { page } from "$app/stores";
   import CountryCard from "../../components/country-card.svelte";
   import { countriesStore } from "../../stores/countries";
 
@@ -17,7 +19,12 @@
   {:then countries}
     <div class="flex justify-center items-center grid grid-cols-2 gap-10 p-3">
       {#each countries as country}
-        <CountryCard countryName={country.name} imgUrl={country.flag_url} />
+        <CountryCard
+          countryName={country.name}
+          imgUrl={country.flag_url}
+          openCard={() =>
+            goto(`${$page.route.id}` + `/${country.name.toLocaleLowerCase()}`)}
+        />
       {/each}
     </div>
   {/await}
