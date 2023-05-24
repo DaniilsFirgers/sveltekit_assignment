1f97a417fbe23e874a10fab86d3f16ee1c484ff2
Daniils Firgers <dfirger@gmail.com>
2023-05-24 22:48:05 +0300

    update counttry-card

--- src/components/country-card.svelte
+++ src/components/country-card.svelte
@@ -1,9 +1,18 @@
 <script lang="ts" setup>
   export let countryName: string;
   export let imgUrl: string;
+  export let openCard: () => void;
+
+  function handleOnClick() {
+    openCard();
+  }
 </script>
 
-<div class="border rounded-lg w-fit p-2 bg-base-200">
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div
+  class="border rounded-lg w-fit p-2 bg-base-200 cursor-pointer"
+  on:click={() => handleOnClick()}
+>
   <h1 class="text-lg font-medium text-center text-primary-focus">
     {countryName}
   </h1>
