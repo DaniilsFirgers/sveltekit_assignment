aba5d901a990935a1d2414c462ca79e69d0f1f56
Daniils Firgers <dfirger@gmail.com>
2023-05-24 22:43:57 +0300

    add global types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,30 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
-	}
+  namespace App {
+    // interface Error {}
+    // interface Locals {}
+    // interface PageData {}
+    // interface Platform {}
+  }
+
+  type CountriesResponse = {
+    countries: Country[];
+  };
+
+  type Country = {
+    name: string;
+    capital: string;
+    flag_url: string;
+    population: number;
+    sightseeing_places: SightseeingPlace[];
+  };
+
+  type SightseeingPlace = {
+    name: string;
+    picture_url: string;
+    description: string;
+  };
 }
 
 export {};
