18a0337e13909e1b785598cf384aa9ae49742d13
Daniils Firgers <dfirger@gmail.com>
2023-06-01 22:14:15 +0300

    add backgound to the page

--- src/routes/countries/[country]/+layout.svelte
+++ src/routes/countries/[country]/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
 </script>
 
-<div class="flex flex-col min-h-screen">
+<div class="flex flex-col min-h-screen bg-base-100">
   <main class="flex flex-1 flex-col p-10">
     <slot />
   </main>
