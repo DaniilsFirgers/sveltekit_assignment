6e11b8ee42157251d5d9ce26d49340b95ab26537
Daniils Firgers <dfirger@gmail.com>
2023-06-01 22:13:51 +0300

    add notes store

---
+++ src/stores/notes.ts
@@ -0,0 +1,17 @@
+import { writable } from "svelte/store";
+import axios from "axios";
+import { goto } from "$app/navigation";
+
+async function initialize(): Promise<any> {
+  try {
+    const res = await axios.get("/notes");
+    const statusCode = res.status;
+    if (statusCode === 200) {
+      return res.data;
+    }
+  } catch (err) {
+    return {};
+  }
+}
+
+export const notesStore = writable(initialize());
