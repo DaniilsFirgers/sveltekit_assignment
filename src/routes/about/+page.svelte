8ed53aea5d3f3102c5e7d46287bd26ceb4c47faa
Daniils Firgers <dfirger@gmail.com>
2023-06-02 15:00:06 +0300

    add +page svelte in home

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,5 +2,16 @@
   <title>About page</title>
   <meta name="description" content="About this app" />
 </svelte:head>
-
-<div class="text-column" />
+<div class="flex flex-col justify-center items-center">
+  <div class="flex mb-20 text-lg">
+    <h1>About the project</h1>
+  </div>
+  <div class="w-1/2">
+    <p>
+      The project demonstrates a part of SvelteKit functionality such as layout
+      and error pages, api server, slugs, load functions, form submissions,
+      stores, project structure and components as a part of Frontend course at
+      the University of Latvia.
+    </p>
+  </div>
+</div>
